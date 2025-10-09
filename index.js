1cee5290926bfc9ffc3b23be23b06cbcd8522e13
joystackinfo <joynelsoninfo@gmail.com>
2025-10-09 20:49:22 +0100

    added mongodb

--- index.js
+++ index.js
@@ -1,5 +1,8 @@
 const express = require('express')
+const mongoose = require('mongoose');
 const app = express()
+
+
 
 app.listen(3000, () => { // listens on port 3000
   console.log('Server is running on port 3000');// console a successfully message
@@ -9,3 +12,12 @@ app.listen(3000, () => { // listens on port 3000
 app.get('/', (req, res) => {  // create a get route
   res.send('Hello running from node api server updated');
 });
+
+
+mongoose.connect('mongodb+srv://joynelsoninfo_db_user:<qG8EY1xOXk7mI5lM>@my-backend.minb8ea.mongodb.net/node-api?retryWrites=true&w=majority&appName=My-backend')
+  .then(() => {
+      console.log('Connected to Database');
+  })
+  .catch(err =>{
+    console.error('Connection failed', err);
+  });
