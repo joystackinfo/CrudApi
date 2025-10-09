c42e8fa361187c17f7a09aed1325444cd98bbba6
joystackinfo <joynelsoninfo@gmail.com>
2025-10-09 19:56:14 +0100

    added comments

--- index.js
+++ index.js
@@ -1,11 +1,11 @@
 const express = require('express')
 const app = express()
 
-app.listen(3000, () => {
-  console.log('Server is running on port 3000');
+app.listen(3000, () => { // listens on port 3000
+  console.log('Server is running on port 3000');// console a successfully message
 })
 
 
-app.get('/', (req, res) => {
-  res.send('Hello from node api');
+app.get('/', (req, res) => {  // create a get route
+  res.send('Hello running from node api server updated');
 });
