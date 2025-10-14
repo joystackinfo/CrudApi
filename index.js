8dc8e110317142754774dc573bf8f826dc8b2afe
joystackinfo <joynelsoninfo@gmail.com>
2025-10-14 22:46:02 +0100

    added a comment

--- index.js
+++ index.js
@@ -24,7 +24,7 @@ app.get('/api/products', async (req, res) => {
 app.get('/api/products/:id', async (req, res) => {
   try {
     const { id } = req.params;
-    const product = await Product.findById(id);
+    const product = await Product.findById(id); // to find the product by id
     res.status(200).json(product);
   } catch (error) {
     res.status(500).json({ message: error.message });
