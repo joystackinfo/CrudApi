06bd8bacac8d78c6e0cd29f79f2d5c8d563d36b9
joystackinfo <joynelsoninfo@gmail.com>
2025-10-24 22:05:43 +0100

    add logs

--- index.js
+++ index.js
@@ -18,7 +18,13 @@ app.use(express.urlencoded({extended:false})) // Allow express to parse urlencod
 // ROUTES
 app.use("/api/products", productRoutes);
 app.use("/api/users", userRoutes);
-//MIDDLEWARE
+
+
+  //FOR LOGGING TO TRACK REQUESTS
+app.use((req, res, next) => {
+  console.log(`${req.method} ${req.url}`);
+  next();
+});
 
 
 // Test route
